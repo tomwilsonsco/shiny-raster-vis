a7e4d592105156cba6bb40c98ff0ae557c3ce724
tomwilsonsco <thomaswilson81@gmail.com>
2022-04-09 18:13:14 +0100

    ext button and band choice alignment

--- app.R
+++ app.R
@@ -9,33 +9,44 @@ raster_use_gray <- raster(raster_file)
 
 
 ui <- fluidPage(
-  selectInput("plot_type", label = "Raster Plot Type", choices = c(rgb = "rgb", grayscale = "grayscale"), selected="rgb"),
-  conditionalPanel(
-    condition = "input.plot_type == 'rgb'",
-    numericInput("red_choice", label = "Red Band", value=3, min = 1, max = 4, step=1),
-    numericInput("green_choice", label = "Green Band",  value=2, min = 1, max = 4, step=1),
-    numericInput("blue_choice", label = "Blue Band", value=1, min = 1, max = 4, step=1)
-  ),
-  conditionalPanel(
-    condition = "input.plot_type == 'grayscale'",
-    selectInput("gray_choice", label = "Grayscale Band", choices = c(1,2,3,4), selected = 1)
-  ),
-  plotOutput("ras_plot", inline=FALSE)
+  selectInput("plot_type", label = "Raster Plot Type", choices = c(rgb = "rgb", grayscale = "grayscale"), selected = "rgb"),
+  fluidRow(
+    conditionalPanel(
+      condition = "input.plot_type == 'rgb'",
+      column(
+        4,
+        numericInput("red_choice", label = "Red Band", value = 3, min = 1, max = 4, step = 1)
+      ),
+      column(
+        4,
+        numericInput("green_choice", label = "Green Band", value = 2, min = 1, max = 4, step = 1)
+      ),
+      column(
+        4,
+        numericInput("blue_choice", label = "Blue Band", value = 1, min = 1, max = 4, step = 1)
+      )
+    ),
+    conditionalPanel(
+      condition = "input.plot_type == 'grayscale'",
+      selectInput("gray_choice", label = "Grayscale Band", choices = c(1, 2, 3, 4), selected = 1)
+    ),
+    actionButton("new_ext", "New extent"),
+    plotOutput("ras_plot", inline = FALSE)
+  )
 )
 
 
 server <- function(input, output, session) {
-  extent_to_use <- generate_random_window(raster_use_rgb)
+  extent_to_use <- eventReactive(input$new_ext, generate_random_window(raster_use_rgb))
+  # extent_to_use <- generate_random_window(raster_use_rgb)
   output$ras_plot <- renderPlot({
     if (input$plot_type == "rgb") {
-      plot_rgb_raster(raster_use_rgb, extent_to_use, image_scale = 1500, red_channel = input$red_choice, green_channel = input$green_choice, blue_channel = input$blue_choice)
-    }
-    else {
-      plot_grayscale_raster(raster_use_gray, extent_to_use, gray_channel=input$gray_choice)
+      plot_rgb_raster(raster_use_rgb, extent_to_use(), image_scale = 1500, red_channel = input$red_choice, green_channel = input$green_choice, blue_channel = input$blue_choice)
+    } else {
+      plot_grayscale_raster(raster_use_gray, extent_to_use(), gray_channel = input$gray_choice)
     }
   })
 }
 
 # Run the application
 shinyApp(ui = ui, server = server)
-
