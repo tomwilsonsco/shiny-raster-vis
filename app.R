8b63497294cfc0b98554686d3b5d5298ef892434
tomwilsonsco <thomaswilson81@gmail.com>
2022-04-11 10:19:56 +0100

    scale range and radio rgb gray

--- app.R
+++ app.R
@@ -7,7 +7,7 @@ raster_use_rgb <- brick(raster_file)
 
 
 ui <- fluidPage(
-  selectInput("plot_type", label = "Raster Plot Type", choices = c(rgb = "rgb", grayscale = "grayscale"), selected = "rgb"),
+  fluidRow(column(width=12, radioButtons("plot_type", label = "Raster Plot Type", choices = c(rgb = "rgb", grayscale = "grayscale"), selected = "rgb", inline=TRUE))),
   fluidRow(
     conditionalPanel(
       condition = "input.plot_type == 'rgb'",
@@ -28,10 +28,10 @@ ui <- fluidPage(
       condition = "input.plot_type == 'grayscale'",
       numericInput("gray_choice", label = "Grayscale Band", value = 1, min = 1, max = 4, step = 1)
     ),
-    fluidRow(column(
-      width = 4,
-      style = "padding-left:30px",
-      actionButton("new_ext", "New extent")
+    fluidRow(column(width=12, style="padding-left:30px;", 
+                    sliderInput("pixel_range", "Pixel min max", min = 0, max = 3000, value = c(0,1500), step = 50))),
+    fluidRow(column(width=12, style="padding-left:30px;",
+      actionButton("new_ext", "New random extent")
     )),
     fluidRow(
       br(),
@@ -43,12 +43,12 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   extent_to_use <- eventReactive(input$new_ext, generate_random_window(raster_use_rgb), ignoreNULL = FALSE)
-  # extent_to_use <- generate_random_window(raster_use_rgb)
   output$ras_plot <- renderPlot({
     if (input$plot_type == "rgb") {
-      plot_rgb_raster(raster_use_rgb, extent_to_use(), image_scale = 1500, red_channel = input$red_choice, green_channel = input$green_choice, blue_channel = input$blue_choice)
+      plot_rgb_raster(raster_use_rgb, extent_to_use(), image_scale = c(input$pixel_range[1], input$pixel_range[2]), 
+                      red_channel = input$red_choice, green_channel = input$green_choice, blue_channel = input$blue_choice)
     } else {
-      plot_grayscale_raster(raster_use_rgb, extent_to_use(), gray_channel = input$gray_choice)
+      plot_grayscale_raster(raster_use_rgb, extent_to_use(), image_scale = input$pixel_range, gray_channel = input$gray_choice)
     }
   })
 }
