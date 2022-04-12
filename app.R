2d8d00598d54d7053ff9050193769d4d7b4dfea0
tomwilsonsco <thomaswilson81@gmail.com>
2022-04-12 10:49:20 +0100

    provisional window size input

--- app.R
+++ app.R
@@ -5,9 +5,13 @@ raster_file <- list.files("data", full.names = TRUE)[1]
 
 raster_use_rgb <- brick(raster_file)
 
+x_dim <- ncol(raster_use_rgb)
+
+y_dim <- nrow(raster_use_rgb)
+
 
 ui <- fluidPage(
-  fluidRow(column(width=12, radioButtons("plot_type", label = "Raster Plot Type", choices = c(rgb = "rgb", grayscale = "grayscale"), selected = "rgb", inline=TRUE))),
+  fluidRow(column(width = 12, radioButtons("plot_type", label = "Raster Plot Type", choices = c(rgb = "rgb", grayscale = "grayscale"), selected = "rgb", inline = TRUE))),
   fluidRow(
     conditionalPanel(
       condition = "input.plot_type == 'rgb'",
@@ -28,9 +32,16 @@ ui <- fluidPage(
       condition = "input.plot_type == 'grayscale'",
       numericInput("gray_choice", label = "Grayscale Band", value = 1, min = 1, max = 4, step = 1)
     ),
-    fluidRow(column(width=12, style="padding-left:30px;", 
-                    sliderInput("pixel_range", "Pixel min max", min = 0, max = 3000, value = c(0,1500), step = 50))),
-    fluidRow(column(width=12, style="padding-left:30px;",
+    fluidRow(
+      column(
+        width = 6, style = "padding-left:30px;",
+        sliderInput("pixel_range", "Pixel min max", min = 0, max = 3000, value = c(0, 1500), step = 50)
+      ),
+      column(width = 3, numericInput("pixels_x", "Window size X (pixels)", min = 100, max = x_dim, value = as.integer(x_dim/4))),
+      column(width = 3, numericInput("pixels_y", "Window size y (pixels)", min = 100, max = y_dim, value = as.integer(y_dim/4)))
+    ),
+    fluidRow(column(
+      width = 12, style = "padding-left:30px;",
       actionButton("new_ext", "New random extent")
     )),
     fluidRow(
@@ -42,11 +53,13 @@ ui <- fluidPage(
 
 
 server <- function(input, output, session) {
-  extent_to_use <- eventReactive(input$new_ext, generate_random_window(raster_use_rgb), ignoreNULL = FALSE)
+  extent_to_use <- eventReactive(input$new_ext, generate_random_window(raster_use_rgb, input$pixels_x, input$pixels_y), ignoreNULL = FALSE)
   output$ras_plot <- renderPlot({
     if (input$plot_type == "rgb") {
-      plot_rgb_raster(raster_use_rgb, extent_to_use(), image_scale = c(input$pixel_range[1], input$pixel_range[2]), 
-                      red_channel = input$red_choice, green_channel = input$green_choice, blue_channel = input$blue_choice)
+      plot_rgb_raster(raster_use_rgb, extent_to_use(),
+        image_scale = c(input$pixel_range[1], input$pixel_range[2]),
+        red_channel = input$red_choice, green_channel = input$green_choice, blue_channel = input$blue_choice
+      )
     } else {
       plot_grayscale_raster(raster_use_rgb, extent_to_use(), image_scale = input$pixel_range, gray_channel = input$gray_choice)
     }
